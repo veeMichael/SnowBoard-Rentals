cbf301e9e1f6ea8087fb625297d90a42a0a93f53
michael <mikevanunu@aol.com>
2023-02-03 23:22:36 -0500

    added files

---
+++ Snowboard Rentals/form1.Designer.cs
@@ -0,0 +1,386 @@
+﻿namespace Snowboard_Rentals
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtIdNum = new System.Windows.Forms.TextBox();
+            this.txtNoBoots = new System.Windows.Forms.TextBox();
+            this.txtYesBoots = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.label14 = new System.Windows.Forms.Label();
+            this.txtCRSNoBoots = new System.Windows.Forms.TextBox();
+            this.txtCRSBoots = new System.Windows.Forms.TextBox();
+            this.txtCRSTotal = new System.Windows.Forms.TextBox();
+            this.txtDRSNoBoots = new System.Windows.Forms.TextBox();
+            this.txtDRSBoots = new System.Windows.Forms.TextBox();
+            this.txtDRSTotal = new System.Windows.Forms.TextBox();
+            this.txtDRSTotalAvg = new System.Windows.Forms.TextBox();
+            this.btnCalcOrder = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnClearAll = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(380, 10);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(137, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Enter Customer Information:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(373, 50);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Name:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(264, 76);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(147, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Driver\'s License / ID Number:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(220, 102);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(191, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Number of Snowboards Without Boots:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(238, 128);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(173, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Number of Snowboards with Boots:";
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(417, 47);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(100, 20);
+            this.txtName.TabIndex = 5;
+            // 
+            // txtIdNum
+            // 
+            this.txtIdNum.Location = new System.Drawing.Point(417, 73);
+            this.txtIdNum.Name = "txtIdNum";
+            this.txtIdNum.Size = new System.Drawing.Size(100, 20);
+            this.txtIdNum.TabIndex = 6;
+            // 
+            // txtNoBoots
+            // 
+            this.txtNoBoots.Location = new System.Drawing.Point(417, 99);
+            this.txtNoBoots.Name = "txtNoBoots";
+            this.txtNoBoots.Size = new System.Drawing.Size(100, 20);
+            this.txtNoBoots.TabIndex = 7;
+            // 
+            // txtYesBoots
+            // 
+            this.txtYesBoots.Location = new System.Drawing.Point(417, 125);
+            this.txtYesBoots.Name = "txtYesBoots";
+            this.txtYesBoots.Size = new System.Drawing.Size(100, 20);
+            this.txtYesBoots.TabIndex = 8;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(130, 234);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(121, 13);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "Current Rental Summary";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(30, 274);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(154, 13);
+            this.label7.TabIndex = 10;
+            this.label7.Text = "Snowboards Without Boots ($):";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(45, 300);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(139, 13);
+            this.label8.TabIndex = 11;
+            this.label8.Text = "Snowboards With Boots ($):";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(101, 326);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(83, 13);
+            this.label9.TabIndex = 12;
+            this.label9.Text = "Rental Total ($):";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(542, 234);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(110, 13);
+            this.label10.TabIndex = 13;
+            this.label10.Text = "Daily Rental Summary";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(488, 274);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(154, 13);
+            this.label11.TabIndex = 14;
+            this.label11.Text = "Snowboards Without Boots ($):";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(506, 300);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(136, 13);
+            this.label12.TabIndex = 15;
+            this.label12.Text = "Snowboards with Boots ($):";
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Location = new System.Drawing.Point(477, 357);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(165, 13);
+            this.label13.TabIndex = 16;
+            this.label13.Text = "Daily Customer Total Average ($):";
+            // 
+            // label14
+            // 
+            this.label14.AutoSize = true;
+            this.label14.Location = new System.Drawing.Point(533, 326);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(109, 13);
+            this.label14.TabIndex = 17;
+            this.label14.Text = "Daily Rental Total ($):";
+            // 
+            // txtCRSNoBoots
+            // 
+            this.txtCRSNoBoots.Location = new System.Drawing.Point(190, 271);
+            this.txtCRSNoBoots.Name = "txtCRSNoBoots";
+            this.txtCRSNoBoots.ReadOnly = true;
+            this.txtCRSNoBoots.Size = new System.Drawing.Size(100, 20);
+            this.txtCRSNoBoots.TabIndex = 18;
+            // 
+            // txtCRSBoots
+            // 
+            this.txtCRSBoots.Location = new System.Drawing.Point(190, 297);
+            this.txtCRSBoots.Name = "txtCRSBoots";
+            this.txtCRSBoots.ReadOnly = true;
+            this.txtCRSBoots.Size = new System.Drawing.Size(100, 20);
+            this.txtCRSBoots.TabIndex = 19;
+            // 
+            // txtCRSTotal
+            // 
+            this.txtCRSTotal.Location = new System.Drawing.Point(190, 323);
+            this.txtCRSTotal.Name = "txtCRSTotal";
+            this.txtCRSTotal.ReadOnly = true;
+            this.txtCRSTotal.Size = new System.Drawing.Size(100, 20);
+            this.txtCRSTotal.TabIndex = 20;
+            // 
+            // txtDRSNoBoots
+            // 
+            this.txtDRSNoBoots.Location = new System.Drawing.Point(648, 271);
+            this.txtDRSNoBoots.Name = "txtDRSNoBoots";
+            this.txtDRSNoBoots.ReadOnly = true;
+            this.txtDRSNoBoots.Size = new System.Drawing.Size(100, 20);
+            this.txtDRSNoBoots.TabIndex = 21;
+            // 
+            // txtDRSBoots
+            // 
+            this.txtDRSBoots.Location = new System.Drawing.Point(648, 297);
+            this.txtDRSBoots.Name = "txtDRSBoots";
+            this.txtDRSBoots.ReadOnly = true;
+            this.txtDRSBoots.Size = new System.Drawing.Size(100, 20);
+            this.txtDRSBoots.TabIndex = 22;
+            // 
+            // txtDRSTotal
+            // 
+            this.txtDRSTotal.Location = new System.Drawing.Point(648, 323);
+            this.txtDRSTotal.Name = "txtDRSTotal";
+            this.txtDRSTotal.ReadOnly = true;
+            this.txtDRSTotal.Size = new System.Drawing.Size(100, 20);
+            this.txtDRSTotal.TabIndex = 23;
+            // 
+            // txtDRSTotalAvg
+            // 
+            this.txtDRSTotalAvg.Location = new System.Drawing.Point(648, 354);
+            this.txtDRSTotalAvg.Name = "txtDRSTotalAvg";
+            this.txtDRSTotalAvg.ReadOnly = true;
+            this.txtDRSTotalAvg.Size = new System.Drawing.Size(100, 20);
+            this.txtDRSTotalAvg.TabIndex = 24;
+            // 
+            // btnCalcOrder
+            // 
+            this.btnCalcOrder.Location = new System.Drawing.Point(118, 490);
+            this.btnCalcOrder.Name = "btnCalcOrder";
+            this.btnCalcOrder.Size = new System.Drawing.Size(89, 23);
+            this.btnCalcOrder.TabIndex = 25;
+            this.btnCalcOrder.Text = "Calculate Order";
+            this.btnCalcOrder.UseVisualStyleBackColor = true;
+            this.btnCalcOrder.Click += new System.EventHandler(this.btnCalcOrder_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(213, 490);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(98, 23);
+            this.btnClear.TabIndex = 26;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnClearAll
+            // 
+            this.btnClearAll.Location = new System.Drawing.Point(317, 490);
+            this.btnClearAll.Name = "btnClearAll";
+            this.btnClearAll.Size = new System.Drawing.Size(98, 23);
+            this.btnClearAll.TabIndex = 27;
+            this.btnClearAll.Text = "Clear All";
+            this.btnClearAll.UseVisualStyleBackColor = true;
+            this.btnClearAll.Click += new System.EventHandler(this.btnClearAll_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(674, 511);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(98, 23);
+            this.btnExit.TabIndex = 28;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 561);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnClearAll);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnCalcOrder);
+            this.Controls.Add(this.txtDRSTotalAvg);
+            this.Controls.Add(this.txtDRSTotal);
+            this.Controls.Add(this.txtDRSBoots);
+            this.Controls.Add(this.txtDRSNoBoots);
+            this.Controls.Add(this.txtCRSTotal);
+            this.Controls.Add(this.txtCRSBoots);
+            this.Controls.Add(this.txtCRSNoBoots);
+            this.Controls.Add(this.label14);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txtYesBoots);
+            this.Controls.Add(this.txtNoBoots);
+            this.Controls.Add(this.txtIdNum);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "SnowBoard Rentals";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtIdNum;
+        private System.Windows.Forms.TextBox txtNoBoots;
+        private System.Windows.Forms.TextBox txtYesBoots;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Label label14;
+        private System.Windows.Forms.TextBox txtCRSNoBoots;
+        private System.Windows.Forms.TextBox txtCRSBoots;
+        private System.Windows.Forms.TextBox txtCRSTotal;
+        private System.Windows.Forms.TextBox txtDRSNoBoots;
+        private System.Windows.Forms.TextBox txtDRSBoots;
+        private System.Windows.Forms.TextBox txtDRSTotal;
+        private System.Windows.Forms.TextBox txtDRSTotalAvg;
+        private System.Windows.Forms.Button btnCalcOrder;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnClearAll;
+        private System.Windows.Forms.Button btnExit;
+    }
+}
+
---
+++ Snowboard Rentals/form1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Snowboard_Rentals
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        const int nBoots = 20;
+        const int yBoots = 30;
+        int drsBoots = 0;
+        int drsnBoots = 0;
+        int drsTotal = 0;
+        int orders = 1;
+
+        
+        
+        private void btnCalcOrder_Click(object sender, EventArgs e)
+        {
+            int noBoots = int.Parse(txtNoBoots.Text) * nBoots;
+            int yesBoots = int.Parse(txtYesBoots.Text) * yBoots;
+            int nyBoots = noBoots + yesBoots;
+
+            txtCRSNoBoots.Text = "" + noBoots;
+            txtCRSBoots.Text = "" + yesBoots;
+            txtCRSTotal.Text = "" + nyBoots;
+
+            drsBoots += yesBoots;
+            drsnBoots += noBoots;
+            drsTotal = (drsBoots +drsnBoots);
+
+            txtDRSBoots.Text = "" + drsBoots;
+            txtDRSNoBoots.Text = "" + drsnBoots;
+            txtDRSTotal.Text = "" + drsTotal;
+            txtDRSTotalAvg.Text = "" + (drsTotal/orders);
+
+            orders++;
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            txtName.Text = "";
+            txtIdNum.Text = "";
+            txtNoBoots.Text = "";
+            txtYesBoots.Text = "";
+            txtCRSBoots.Text = "";
+            txtCRSNoBoots.Text = "";
+            txtCRSTotal.Text = "";
+        }
+
+        private void btnClearAll_Click(object sender, EventArgs e)
+        {
+
+            txtName.Text = "";
+            txtIdNum.Text = "";
+            txtNoBoots.Text = "";
+            txtYesBoots.Text = "";
+            txtCRSBoots.Text = "";
+            txtCRSNoBoots.Text = "";
+            txtCRSTotal.Text = "";
+            txtDRSBoots.Text = "";
+            txtDRSNoBoots.Text = "";
+            txtDRSTotal.Text = "";
+            txtDRSTotalAvg.Text = "";
+
+            drsBoots = 0;
+            drsnBoots = 0;
+            drsTotal = 0;
+            orders = 1;
+
+            
+            
+        }
+    }
+}
